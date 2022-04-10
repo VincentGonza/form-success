930222cc5f31f9ddf21c539566b595072e5aa939
Vincent Gonzalez <68647704+VincentGonza@users.noreply.github.com>
2022-04-10 00:08:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ $(document).on('click','.navbar-collapse',function(e) {
     if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
         $(this).collapse('hide');
     }
+
+    
 });
+
+function scrollBottom() {window.scrollTo(0, 99999);}
+if (document.addEventListener) document.addEventListener("DOMContentLoaded", scrollBottom, false)
+else if (window.attachEvent) window.attachEvent("onload", scrollBottom);
